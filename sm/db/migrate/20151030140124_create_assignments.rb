5e50b0ade2df56c569fcd1caa831ca51ada506a0
Alan Sun <alanysun@gmail.com>
2015-11-03 14:29:19 -0500

    Creating Database

--- sm/db/migrate/20151030140124_create_assignments.rb
+++ sm/db/migrate/20151030140124_create_assignments.rb
@@ -1,6 +1,10 @@
 class CreateAssignments < ActiveRecord::Migration
   def change
     create_table :assignments do |t|
+    	t.string :discussion_title
+    	t.datetime :due_date
+    	t.string :discussion_question
+    	t.string :edit_question
 
       t.timestamps null: false
     end
