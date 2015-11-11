b63b3cb779f0f5005898ef2608a7e71033070324
Alan Sun <alanysun@gmail.com>
2015-11-11 08:28:17 -0500

    added files

--- sm/db/migrate/20151105174024_create_responses.rb
+++ sm/db/migrate/20151105174024_create_responses.rb
@@ -2,7 +2,7 @@ class CreateResponses < ActiveRecord::Migration
   def change
     create_table :responses do |t|
     	t.text :student_responses
-    	t.
+    	
 
       t.timestamps null: false
     end
