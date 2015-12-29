8ce98c2142d458e3a4c10da77b53d502c9cc0522
kenharsch <kharsch@brandeis.edu>
2015-12-28 21:28:11 -0500

    changes to gems and config for test deployment to server

--- sm/app/assets/javascripts/application.js
+++ sm/app/assets/javascripts/application.js
@@ -10,10 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require ckeditor/init
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
 //= require bootstrap-sprockets
 //= require bootstrap
-//= require ckeditor/init
 //= require_tree .
